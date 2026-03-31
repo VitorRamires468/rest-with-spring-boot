e0173485b568fa5b1caa20ca37c9631c7711cb14
vitorramires468 <ramiresvitor468@gmail.com>
2026-03-30 23:20:49 -0300

    Logs in Spring Boot Applications

---
+++ 08_LogsInSpringBoot/src/main/java/br/com/vitorramires468/controllers/TestLogController.java
@@ -0,0 +1,24 @@
+package br.com.vitorramires468.controllers;
+
+import br.com.vitorramires468.services.PersonServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class TestLogController {
+
+    private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
+
+    @GetMapping("/test")
+    public String testLog(){
+        logger.debug("This is a DEBUG log");
+        logger.info("This is an INFO log");
+        logger.warn("This is a WARN log");
+        logger.error("This is an ERROR log");
+        return "Logs generated successfully";
+    }
+}
---
+++ 08_LogsInSpringBoot/src/main/java/br/com/vitorramires468/services/PersonServices.java
@@ -0,0 +1,62 @@
+package br.com.vitorramires468.services;
+
+import br.com.vitorramires468.exceptions.ResourceNotFoundException;
+import br.com.vitorramires468.model.Person;
+import br.com.vitorramires468.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    PersonRepository personRepository;
+
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
+
+    public Person findById(Long id){
+        logger.info("Find one Person");
+
+        return personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No record found for this ID!"));
+    }
+
+    public List<Person> findAll(){
+
+        logger.info("Find all");
+
+        return personRepository.findAll();
+    }
+
+    public Person create(Person person){
+        logger.info("Creating one Person");
+        return personRepository.save(person);
+    }
+
+    public Person update(Person person){
+        logger.info("Updating one Person");
+
+        Person entity = personRepository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No record found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return personRepository.save(entity);
+    }
+
+    public void delete(Long id){
+        logger.info("Deleting one Person");
+        Person entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No record found for this ID!"));
+
+        personRepository.delete(entity);
+    }
+
+}
